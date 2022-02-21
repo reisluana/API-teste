f60e327a61ece4619f6e4abdf88bb477c0a6bda5
Luana Reis <reisluanag@gmail.com>
2022-02-21 17:07:32 -0300

    corrigindo bugs de integração

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app)
+
+  const config = new DocumentBuilder()
+    .setTitle('API teste')
+    .setDescription('Pesquisar publicação ou autor')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
